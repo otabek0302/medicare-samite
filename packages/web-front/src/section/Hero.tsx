89f265999d68f168107fee25db291804853282e8
AzamatUzakov <azamatakmal053@gmail.com>
2025-04-01 18:27:20 +0500

    finish hero

--- packages/web-front/src/section/Hero.tsx
+++ packages/web-front/src/section/Hero.tsx
@@ -10,16 +10,20 @@ const Hero: React.FC<HeroProps> = () => {
 
         <>
         
-            <div className="flex flex-col bg-[#041150] pt-7 px-2">
+            <div className="bg-[#041150] pt-7 px-2 md:flex items-center md:px-7 pb-20 lg:pb-0">
                 
                 <div>
-                    <h2 className="text-xl text-white font-normal mt-3">Welcome to SAMIT</h2>
-                    <h1 className="text-[#34C38F] text-3xl font-semibold mt-1 mb-4">We Are Providing Best & Affordable Health Care.</h1>    
+                    <h2 className="text-xl text-white font-normal mt-3 md:text-2xl lg:text-2xl">Welcome to SAMIT</h2>
+                    <h1 className="text-[#34C38F] text-3xl font-semibold mt-1 mb-4 md:text-4xl md:text-5xl">We Are Providing Best & Affordable Health Care.</h1>    
                     <p className="text-[16px] text-[#ffffff80]"> Experience Unmatched Healthcare Excellence at  SAMIT : Comprehensive Medical Services, Advanced Hospital Management, and Compassionate Patient Care for a Healthier Tomorrow</p>
-               
-                    <div className="flex justify-start mt-5 gap-5"><Button className="w-[170px] border border-white text-center text-sm h-[40px] bg-white text-[#041150] font-medium">Get Started</Button> <Button className="w-[170px] border text-sm border-white text-center h-[40px] bg-[#10101000] font-medium">Get Appointment</Button></div> </div>
             
+                    <div className="flex justify-start mt-5 gap-2"><Button className="w-[170px] border border-white text-center text-[16px] h-[40px] bg-white text-[#041150] font-medium">Get Started</Button> <Button className="w-[170px] border text-[16px] border-white text-center h-[40px] bg-[#10101000] font-medium">Get Appointment</Button></div>
+                </div>
+            
+                <img src="https://api.medicare.samit.global/public/storage/configurations/2024-12-13-675c754d5c424.png" alt="photo" className="w-[80%]  mt-12 mx-auto md:w-[50%] md:mt-0 lg:w-[80%]" />
+                
             </div>
+
 
             
         </>
