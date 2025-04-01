2af1b5f8f9eacd360dc1ba2ebd6d63445cabec40
Feruz <140828663+litakk@users.noreply.github.com>
2025-04-01 17:41:04 +0500

    created header

--- packages/web-front/src/components/custom/Header.tsx
+++ packages/web-front/src/components/custom/Header.tsx
@@ -1,13 +1,33 @@
 import React from "react";
-
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+  
  
 const Header: React.FC = () => {
     return ( <>
     <div>
 
-<div className="bg-[#07417f] w-full text-white font-bold flex p-[10px]">
+<div className="bg-[#041150] w-full text-white font-bold flex items-center justify-center py-[10px] px-5 gap-9 md:justify-around ">
+    <div className="flex md:gap-2">
     <img src="/logo.png" alt="logo.png" className="w-[40px] h-[32px]" />
     <h1>SAMIT</h1>
+    </div>
+
+    <Select>
+  <SelectTrigger className="w-[80px] bg-white text-black ml-auto">
+    <SelectValue placeholder="EN" />
+  </SelectTrigger>
+  <SelectContent>
+    <SelectItem value="light">RU</SelectItem>
+    <SelectItem value="dark">UZ</SelectItem>
+    <SelectItem value="system">EN</SelectItem>
+  </SelectContent>
+</Select>
 </div>
 
     </div>
