c7d2a47a969716258990d7e22afd5f2e0f1029eb
Feruz <140828663+litakk@users.noreply.github.com>
2025-04-01 17:12:21 +0500

    creating header

--- packages/web-front/src/components/custom/Header.tsx
+++ packages/web-front/src/components/custom/Header.tsx
@@ -3,7 +3,14 @@ import React from "react";
  
 const Header: React.FC = () => {
     return ( <>
-    <h1>header</h1>
+    <div>
+
+<div className="bg-[#07417f] w-full text-white font-bold flex p-[10px]">
+    <img src="/logo.png" alt="logo.png" className="w-[40px] h-[32px]" />
+    <h1>SAMIT</h1>
+</div>
+
+    </div>
     </> );
 }
  
